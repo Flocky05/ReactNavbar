be2d2af52f3c68044b68e70de388e94bac2bed36
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-21 01:14:50 +0600

    navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="flex bg-black text-white justify-center gap-14 p-6 text-xl">
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/blog">Blog</a>
+            <a href="/contact">Contact</a>
+        </div>
+    );
+};
+
+export default Navbar;
